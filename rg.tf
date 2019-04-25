25701e058c83b9eb0912fbd8f762c030877d2c79
Mihai Cristian Satmarean <msn3@3ds.com>
2019-04-25 13:57:16 +0200

    changed the resource group

--- rg.tf
+++ rg.tf
@@ -6,3 +6,15 @@ resource "azurerm_resource_group" "test" {
     environment = "DONT RUN DESTROY"
   }
 }
+
+# Create virtual network
+resource "azurerm_virtual_network" "myterraformnetwork" {
+    name                = "myVnet"
+    address_space       = ["10.0.0.0/16"]
+    location            = "eastus"
+    resource_group_name = "${azurerm_resource_group.test.name}"
+
+    tags {
+        environment = "Terraform Demo"
+    }
+}
