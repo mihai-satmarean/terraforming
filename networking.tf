01f9c71050f9c7943dc09fa75e405bf5149c21b5
Mihai Cristian Satmarean <msn3@3ds.com>
2019-04-25 15:11:27 +0200

    add public IP

--- networking.tf
+++ networking.tf
@@ -20,3 +20,17 @@ resource "azurerm_subnet" "myterraformsubnet" {
     virtual_network_name = "${azurerm_virtual_network.myterraformnetwork.name}"
     address_prefix       = "10.0.1.0/24"
 }
+
+
+
+# Create public IPs
+resource "azurerm_public_ip" "myterraformpublicip" {
+    name                         = "myPublicIP"
+    location                     = "westeurope"
+    resource_group_name          = "${azurerm_resource_group.test.name}"
+    allocation_method            = "Dynamic"
+
+    tags {
+        environment = "run whatever you want"
+    }
+}
